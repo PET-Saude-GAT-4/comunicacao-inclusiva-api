4f5a5a7249765b333392be70acbfb6b0bb5bdf50
NicoStone-dev <nicostone.rocha@gmail.com>
2026-03-14 01:35:36 -0300

    fix|feat: move prisma into RootDir, implement base repo interface

--- src/repositories/role/IRoleRepository.ts
+++ src/repositories/role/IRoleRepository.ts
@@ -1,3 +1,7 @@
-interface IRoleRepository {}
+import type { Role } from "@/models/Role.js";
+
+import type { IRepository } from "../IRepository.js";
+
+interface IRoleRepository extends IRepository<Role> {}
 
 export type { IRoleRepository };
--- src/repositories/user/IUserRepository.ts
+++ src/repositories/user/IUserRepository.ts
@@ -1,3 +1,12 @@
-interface IUserRepository {}
+import type { User } from "@/models/User.js";
+
+import type { IRepository } from "../IRepository.js";
+
+interface IUserRepository extends IRepository<User> {
+    findByEmail(email: string): Promise<User>;
+    findByUuid(uuid: string): Promise<User>;
+    existsByEmail(email: string): Promise<boolean>;
+    existsByUuid(uuid: string): Promise<boolean>;
+}
 
 export type { IUserRepository };
